5e797955c37c0f5caa03777946f47594130b89d5
Victor Heringer <victor.silva@meliuz.com.br>
2025-04-02 11:11:35 -0300

    Init project

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, SafeAreaView } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
